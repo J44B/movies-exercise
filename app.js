94f416692410e9a09e09beb7b705df8b44c32af7
Yevhen <eugene.shust@gmail.com>
2024-10-09 16:14:04 +0200

    Added header without styles.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // main file to execute functions
-
+import { createHeader } from "./modules/ui.js";
 import { fetchMovies, fetchPopularMovies } from './modules/index.js';
 
+document.body.insertAdjacentElement('afterbegin', createHeader());
+// document.body.insertAdjacentElement('beforeend', createFooter());
 fetchMovies(); // fetch for use on manual search
 fetchPopularMovies(); // fetch for use to display default cards on homepage
