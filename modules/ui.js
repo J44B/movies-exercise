af42ecf08a6ffb83857a06e2e5c0aae48723ab00
Kivikk <kontakt@kuvik.de>
2024-10-10 17:44:21 +0200

    add button to ui.js, add appendChild for all infos on card

--- modules/ui.js
+++ modules/ui.js
@@ -2,10 +2,11 @@
 export function createCard(data) {
   const card = document.createElement('div');
 
+
   card.classList.add(
     'bg-slate-800',
     'shadow-lg',
-    'h-48',
+    'h-64',
     'w-40',
     'border-solid',
     'border-1',
@@ -13,10 +14,11 @@ export function createCard(data) {
     'flex',
     'flex-row',
     'text-sm',
-    'rounded-t'
+    'rounded-t',
+    'm-2'
   );
   const movieImage = document.createElement('img');
-  movieImage.src = data.image;
+  movieImage.src = data.poster_path;
   movieImage.classList.add(
     'h-2/3',
     'w-full',
@@ -25,18 +27,58 @@ export function createCard(data) {
     'shadow-slate-400'
   );
 
-  const movieTitel = document.createElement('p');
-  movieTitel.textContent = data.title;
-  movieTitel.classList.add(
+  const movieTitle = document.createElement('p');
+  movieTitle.textContent = data.title;
+  movieTitle.classList.add(
     'truncate',
-    'white',
+    'text-white',
     'font-bold',
     'p-2',
     'mt-2'
   )
-  const movieRunTime = document.createElement('p');
-  movieRunTime.textContent = `${data.runtTime} minutes`;
-  movieRunTime.classList.add(
-    'text-'
-  )
+  const movieGenre = document.createElement('p');
+  movieGenre.textContent = data.genres;
+  movieGenre.classList.add(
+    'text-white',
+    'p-2',
+    'mt-2'
+  );
+
+  const movieRelease = document.createElement('p');
+  movieRelease.textContent = data.release_date;
+  movieRelease.classList.add(
+    'text-white',
+    'p-2'
+  );
+
+  const addToJournalBtn = document.createElement('button');
+  addToJournalBtn.textContent = 'Add to Journal';
+  addToJournalBtn.classList.add(
+    'bg-amber-600',
+    'border-solid',
+    'border-slate-300',
+    'rounded',
+    'w-1/2',
+    'p-1',
+    'text-xs',
+    'text-orange-100',
+    'mx-auto',
+    'mb-1',
+    'hover:bg-amber-800',
+    'active:bg-amber-950'
+  );
+  addToJournalBtn.onclick = () => {
+    addToLocalStorage('card', data);
+  };
+
+
+
+
+  card.appendChild(movieImage);
+  card.appendChild(movieTitle);
+  card.appendChild(movieGenre);
+  card.appendChild(movieRelease);
+  card.appendChild(addToJournalBtn);
+
+  return card;
 }
