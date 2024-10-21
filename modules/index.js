b74eefd12aa52b0dbd1a16c25f48d816c03c3dc4
Yevhen <eugene.shust@gmail.com>
2024-10-21 13:13:49 +0200

    Exporting components is simplified.

--- modules/index.js
+++ modules/index.js
@@ -2,34 +2,13 @@
 
 // --- Begin import section ---
 
-import { fetchMovies, fetchPopularMovies } from './fetch.js';
-import { addFavorite, removeFavorite, getFavorites } from './storage.js';
-import {
+export { fetchMovies, fetchPopularMovies } from './fetch.js';
+export { addFavorite, removeFavorite, getFavorites } from './storage.js';
+export {
   createHeader,
   createFooter,
   createCard,
   createJournalCard,
   lupe,
 } from './ui.js';
-import { searchInJournal } from './search.js';
-// --- End import section ---
-
-// --- Begin export section ---
-
-export {
-  fetchMovies,
-  fetchPopularMovies,
-  addFavorite,
-  removeFavorite,
-  getFavorites,
-  createHeader,
-  createFooter,
-  createCard,
-  createJournalCard,
-  searchInJournal,
-  lupe,
-};
-// export { addFavorite, removeFavorite, getFavorites }
-// export { createHeader, createFooter }
-
-// --- Begin export section ---
+export { searchInJournal } from './search.js';
